38487454dc35321172878555d03104cedf552ceb
IDGS-801-20001541 <domdaniela85@gmail.com>
2023-01-26 19:15:57 -0600

    26-enero

--- actividad2.py
+++ actividad2.py
@@ -5,21 +5,23 @@ class Calculadora:
     n1=0
     n2=0
     res=0
+    
     #constructor de clase
-    def _init_(self, a,b) :
+    def _init_(self, a,b) -> None :
         self.n1 = a
         self.n2 = b
 
     #los metodos de clase
-    def suma(self):
-        self.res=self.n1+self.n2
-    def resta(self):
-        self.res=self.n1-self.n2
-    def multiplicacion(self):
-        self.res=self.n1*self.n2
-    def division(self):  
+    def suma(self, a,b):
+        self.res=a+b
+        
+    def resta(self, a,b):
+        self.res=a-b
+    def multiplicacion(self, a,b):
+        self.res=a*b
+    def division(self, a,b):  
         try: 
-            self.res=self.n1/self.n2
+            self.res=a/b
         except ZeroDivisionError:
             print("No se puede diviidir entre 0 :'(")
 
@@ -30,33 +32,42 @@ def main():
     op =0 
     while True: 
         print(" Menu \n 1- Suma \n 2- Resta \n 3- Multiplicación  \n 4- Division \n 5- Salir")    
-op = int(input("Elige la opción "))
-    if op == 1:
-        nu1= float(input("Escriba el primer número "))
-        nu2= float(input("Escriba el primer número "))
 
-        obj=Calculadora(nu1,nu2)
-        obj.suma()
-        print("La suma es: {}".format(obj.res))
-       
-    elif op == 3:
-        nu1= float(input("Escriba el primer número "))
-        nu2= float(input("Escriba el primer número "))
+        op = int(input("Elige la opción "))
+    
+        if op == 1:
+            nu1= float(input("Escriba el primer número "))
+            nu2= float(input("Escriba el primer número "))
 
-        obj=Calculadora(nu1,nu2)
-        obj.multiplicacion()
-        print("La multiplicación es: {}".format(obj.res))
+            obj=Calculadora()
+            obj.suma(nu1,nu2)
+            print("La suma es: {}".format(obj.res))
+        elif op == 2:
+            nu1= float(input("Escriba el primer número "))
+            nu2= float(input("Escriba el primer número "))
+
+            obj=Calculadora()
+            obj.resta(nu1,nu2)
+            print("La Resta es: {}".format(obj.res))
+
+        elif op == 3:
+            nu1= float(input("Escriba el primer número "))
+            nu2= float(input("Escriba el primer número "))
+
+            obj=Calculadora()
+            obj.multiplicacion(nu1,nu2)
+            print("La multiplicación es: {}".format(obj.res))
 
 
-    elif op == 4:
-        nu1= float(input("Escriba el primer número "))
-        nu2= float(input("Escriba el primer número "))
+        elif op == 4:
+            nu1= float(input("Escriba el primer número "))
+            nu2= float(input("Escriba el primer número "))
 
-        obj=Calculadora(nu1,nu2)
-        obj.division()
-        print("La resta es: {}".format(obj.res)) 
-    else:
-        exit
+            obj=Calculadora()
+            obj.division(nu1,nu2)
+            print("La resta es: {}".format(obj.res)) 
+        else:
+            exit
 
 if __name__ == '__main__':
     main()
